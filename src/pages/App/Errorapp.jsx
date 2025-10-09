e2a793f882cb82ef81cbe9b2ab2738c78d34009a
hamidur800 <hamidur800t@gmail.com>
2025-10-09 20:38:44 +0600

    Allhamdullah_App-All-fauction_Done

--- src/pages/App/Errorapp.jsx
+++ src/pages/App/Errorapp.jsx
@@ -3,14 +3,14 @@ import AppError from "../../../public/App-Error.png";
 
 export default function ErrorApp() {
   return (
-    <div className="flex flex-col items-center justify-center py-20 text-center max-w-5/12 mx-auto">
-      <img
-        src={AppError}
-        alt="No App Found"
-        className="w-[100%] h-[100%] mb-6"
-      />
-      <h2 className="text-5xl font-bold">OPPS!! APP NOT FOUND</h2>
-      <p className="text-gray-500 mt-2">
+    <div className="flex flex-col items-center justify-center py-20 text-center mx-auto">
+      <div className="w-1/2 lg:w-1/5">
+        <img src={AppError} alt="No App Found" className=" mb-6" />
+      </div>
+      <h2 className="text-5xl font-bold text-[#001931] mb-2 mt-4">
+        OPPS!! APP NOT FOUND
+      </h2>
+      <p className="text-xl text-[#627382] mb-4">
         The App you are requesting is not found on our system. please try
         another apps
       </p>
