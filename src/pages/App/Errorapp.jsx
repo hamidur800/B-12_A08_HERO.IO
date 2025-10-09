cc2266a26daea4cc7b7e15aefc2347b58a11fda1
hamidur800 <hamidur800t@gmail.com>
2025-10-09 19:26:56 +0600

    Allhamdullah_just-instal_Holaii-Done

--- src/pages/App/Errorapp.jsx
+++ src/pages/App/Errorapp.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router";
 import AppError from "../../../public/App-Error.png";
 
 export default function ErrorApp() {
@@ -8,10 +9,14 @@ export default function ErrorApp() {
         alt="No App Found"
         className="w-[100%] h-[100%] mb-6"
       />
-      <h2 className="text-2xl font-semibold text-red-500">🚫 No Apps Found</h2>
+      <h2 className="text-5xl font-bold">OPPS!! APP NOT FOUND</h2>
       <p className="text-gray-500 mt-2">
-        Try searching something else or check back later.
+        The App you are requesting is not found on our system. please try
+        another apps
       </p>
+      <Link className="bg-gradient-to-tl from-[#9F62F2] to-[#632EE3] py-2 px-3 rounded-sm text-white">
+        Go Back!
+      </Link>
     </div>
   );
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { NavLink, useLoaderData } from "react-router";
+import { Link, NavLink, useLoaderData } from "react-router";
 import google from "../../../public/g-stor.png";
 import apple from "../../../public/a-stor.png";
 import hero from "../../../public/hero.png";
@@ -105,53 +105,55 @@ const Home = () => {
             Explore All Trending Apps on the Market developed by us
           </h4>
         </div>
-        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 max-w-11/12 mx-auto">
-          {data.map((data) => (
-            <div key={data.id} className=" bg-white rounded-xl shadow p-3">
-              {/* Image Placeholder */}
-              <div className="flex items-center justify-center rounded-lg">
-                <img src={data.image} alt="" />
-              </div>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 max-w-11/12 mx-auto pb-5">
+          {data.map((app) => (
+            <div key={app.id} className="">
+              <Link to={`/Appdetails/${app.id}`}>
+                <div className=" bg-white rounded-xl shadow p-3">
+                  {/* Image Placeholder */}
+                  <div className="flex items-center justify-center rounded-lg">
+                    <img src={app.image} alt="" />
+                  </div>
 
-              {/* Title */}
-              <h3 className="mt-3 text-sm font-semibold text-gray-800">
-                {data.title}
-              </h3>
+                  {/* Title */}
+                  <h3 className="mt-3 text-sm font-semibold text-gray-800">
+                    {app.title}
+                  </h3>
 
-              {/* Footer */}
-              <div className="flex justify-between items-center mt-2 text-xs">
-                <span className="flex items-center text-green-500 font-semibold">
-                  {/* Download icon */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 mr-1"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M12 4v12m0 0l-3-3m3 3l3-3m-9 7h12"
-                    />
-                  </svg>
-                  {data.downloads}M
-                </span>
+                  {/* Footer */}
+                  <div className="flex justify-between items-center mt-2 text-xs">
+                    <span className="flex items-center text-green-500 font-semibold">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4 mr-1"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M12 4v12m0 0l-3-3m3 3l3-3m-9 7h12"
+                        />
+                      </svg>
+                      {app.downloads}M
+                    </span>
 
-                <span className="flex items-center text-orange-500 font-semibold">
-                  {/* Star icon */}
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-4 w-4 mr-1"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.283L12 18.896l-7.416 4.521 1.48-8.283-6.064-5.828 8.332-1.151z" />
-                  </svg>
-                  {data.ratingAvg}
-                </span>
-              </div>
+                    <span className="flex items-center text-orange-500 font-semibold">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-4 w-4 mr-1"
+                        fill="currentColor"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.283L12 18.896l-7.416 4.521 1.48-8.283-6.064-5.828 8.332-1.151z" />
+                      </svg>
+                      {app.ratingAvg}
+                    </span>
+                  </div>
+                </div>
+              </Link>
             </div>
           ))}
         </div>
